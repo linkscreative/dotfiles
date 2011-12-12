eb30fef658ab505a029c2b2f6bebbac2319ef414
James Cleveland <jamescleveland@gmail.com>
2011-12-12 16:20:04 +0000

    fixed path bugs

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
 export BASE=$(dirname $0)
+BASE=${BASE:a}
 cd $BASE && git pull origin master
+
 manifest=(
     .gitconfig
     .tmux.conf
@@ -15,17 +17,20 @@ manifest=(
 relink () {
     real=$BASE/$1
     real=${real:a}
-    new=$BASE/../$1
-    new=${new:a}
+    new=$HOME/$1
     newdir=${new:h}
     [[ -d $newdir ]] || mkdir -p $newdir
-    [[ -a $new ]] && ( unlink $new || rm $new )
+    [[ -h $new ]] && unlink $new
+    [[ -f $new ]] && rm $new
     ln -s $real $new && echo "linked $new -> $real"
 }
+
 [[ -f $BASE/../.tmux.hotkey ]] || cp $BASE/.tmux.hotkey.sample $BASE/../.tmux.hotkey
 
 git clone git://github.com/zsh-users/zsh-history-substring-search.git $BASE/.zsh/history-substring-search 1&>/dev/null || (cd $BASE/.zsh/history-substring-search && git pull origin master)
+
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $BASE/.zsh/syntax-highlighting 1&>/dev/null || (cd $BASE/.zsh/syntax-highlighting && git pull origin master)
+
 for x in $manifest
 do
     relink $x
