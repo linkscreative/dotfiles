a1c7853301450307dbbc7c65ca079bdb65a02113
James Cleveland <jamescleveland@gmail.com>
2012-08-23 21:58:32 +0100

    use default faster dmenu

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -11,8 +11,7 @@ myLayout = avoidStruts(spacing 5 $ tiled ||| Mirror tiled) |||
     where
         tiled = ResizableTall 1 (3/100) (1/2) []
 
-myMenu = "dmenu_run -fn 'terminus-9'"
-    ++ " -sb '#dd1144' -nb '#000000'"
+myMenu = "dmenu_run"
 
 myLockscreen = "slimlock"
 
